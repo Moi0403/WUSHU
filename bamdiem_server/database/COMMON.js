6554882d69ed95aad4fde61c7916473e6ec467b3
Moi0403 <nta2002.bap@gmail.com>
2024-10-04 15:23:50 +0700

    4/10

--- bamdiem_server/database/COMMON.js
+++ bamdiem_server/database/COMMON.js
@@ -1,8 +1,12 @@
-const uri = 'mongodb+srv://nta2004:PI4DVozAR1HER8zf@cluster0.xopjfar.mongodb.net/bdiem';
-module.exports = {
-    uri: uri,
-}
+// const uri = 'mongodb+srv://nta2004:PI4DVozAR1HER8zf@cluster0.xopjfar.mongodb.net/bdiem';
+// module.exports = {
+//     uri: uri,
+// }
 // const uri = 'mongodb+srv://vucodu:cugJyHGWmESqOfUF@cluster0.ufm9hop.mongodb.net/wushu';
 // module.exports = {
 //     uri: uri,
 // }
+const uri = 'mongodb+srv://wushu2024:3hyy9CJb6qJ7qXKL@cluster0.v6f46.mongodb.net/wushu';
+module.exports = {
+    uri: uri,
+}
